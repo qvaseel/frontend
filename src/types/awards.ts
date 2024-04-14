7b90839959a89845360590916e803b2415e71b83
qvaseel <118725399+qvaseel@users.noreply.github.com>
2024-04-14 16:31:00 +0500

    Add files via upload

---
+++ src/types/awards.ts
@@ -0,0 +1,11 @@
+import img1 from "../../src/img/awards/award_tspk.webp"
+import img2 from "../../src/img/awards/award_blag.webp"
+
+export const enum_awards = [
+    {
+        img: img1
+    },
+    {
+        img: img2
+    }
+]
